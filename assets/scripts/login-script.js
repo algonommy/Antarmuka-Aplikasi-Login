937712ab20e7922144f23d834501e04835364646
Algoritma & Economy <91531966+algonommy@users.noreply.github.com>
2022-02-13 21:52:00 +0700

    Update login-script.js
    Mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,53 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
-/* comment : Membuat variabel untuk setiap elemen view. */
+/** @module Login-Script */
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
-/* comment : Membuat variabel untuk menyimpan informasi email dan password. */
+
+/** 
+ * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
+
+
+/** 
+ * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
+
 
 /* comment : Menambahkan aksi klik pada button */
 loginFormElement.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  /* comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+  /** 
+   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const email = inputEmailElement.value;
+
+  /** 
+   * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const password = inputPasswordElement.value;
 
   /* TODO 2 : Buat Logika perbandingan dengan kondisi:
@@ -28,12 +56,12 @@ loginFormElement.addEventListener('submit', function (event) {
    */
   /* comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
-    
+
     /* comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
 
   } else {
-    
+
     /* comment : Namun jika tidak sesuai maka akan menampilkan bahwa input salah. */
     showPopUp();
   }
