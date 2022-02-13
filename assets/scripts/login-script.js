22d1b95ef5ffdfa2ccbeebe9b983304c01003b88
Algoritma & Economy <91531966+algonommy@users.noreply.github.com>
2022-02-13 21:29:40 +0700

    Update login-script.js
    Menambahkan comment pada setiap source code yang diperlukan agar source code dapat dengan mudah dipahami.

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,16 +5,20 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/* comment : Membuat variabel untuk setiap elemen view. */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/* comment : Membuat variabel untuk menyimpan informasi email dan password. */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
-loginFormElement.addEventListener('submit', function(event) {
-    event.preventDefault();
-  // TODO 1 : Mendapatkan input email dan password pengguna dari form.
+/* comment : Menambahkan aksi klik pada button */
+loginFormElement.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  /* comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
   const email = inputEmailElement.value;
   const password = inputPasswordElement.value;
 
@@ -22,10 +26,15 @@ loginFormElement.addEventListener('submit', function(event) {
      Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
      Jika tidak, maka panggil fungsi showPopUp().
    */
+  /* comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
+    
+    /* comment : Jika sesuai maka program akan berpindah ke halaman home. */
     goToHome();
+
   } else {
+    
+    /* comment : Namun jika tidak sesuai maka akan menampilkan bahwa input salah. */
     showPopUp();
   }
-
 });
